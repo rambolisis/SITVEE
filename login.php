63b378deb604c9681fecbe5dcf656f99c894a294
Andrey Palma Jimenez <andreypj98@gmail.com>
2018-07-15 22:28:12 -0600

    Login con sessions y alert basico en caso de error

--- login.php
+++ login.php
@@ -1,14 +1,15 @@
 <title>Login</title>
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/login.css">
-<script src="js/login.js" type="text/javascript"></script>
+<script src="js/jquery-3.3.1.min.js"></script>
 <?php include('template/header.php'); ?>
-            <form id="loginForm" action="validar.php" action="login.js" method="post">
+            <form action="validar.php" method="POST">
                 <h2>Bienvenido a Sitvee</h2>
-                <input type="text" placeholder=" Usuario" name="usuario"/>
-                <input type="password" placeholder=" Contraseña" name="clave"/>
-                <input type="submit" value="Ingresar" />
+                <input type="text" name="usuario" placeholder=" Usuario">
+                <input type="password" name="clave" placeholder=" Contraseña">
+                <input type="submit" value="Iniciar Sesion">
             </form>
+            
         <?php include('template/footer.php'); ?>
     </body>
 </html>
