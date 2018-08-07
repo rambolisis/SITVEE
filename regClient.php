bb041653fab02955c62c5c13950e47167ebde63f
AndreyPalma <andreypj98@gmail.com>
2018-08-07 14:57:44 -0600

    Insertar cliente a base de datos arreglado

--- regClient.php
+++ regClient.php
@@ -1,22 +1,27 @@
 <?php
-    $conn = new mysqli('localhost','id6607651_sitvee','12345','id6607651_sitvee');
+    $mysqli = new mysqli('localhost','id6607651_sitvee','12345','id6607651_sitvee');
 
-    if($conn->connect_error){
-        die($conn->connection_error);
+    if($mysqli->connect_error){
+        die($mysqli->connection_error);
     }
 	//obtenemos los valores del formulario
 	$nombreCliente = $_POST['nombreCliente'];
-	$usuarioCliente = $_POST['usuarioCliente'];
-	$emailCliente = $_POST['correoCliente'];
+    $emailCliente = $_POST['correoCliente'];
+    $usuarioCliente = $_POST['usuarioCliente'];
     $pass = $_POST['contraseniaCliente'];
     
-    $sql = "INSERT INTO cliente(id_Cliente, nombre, correo, id_Usuario) VALUES ('','$nombreCliente','$emailCliente',3)";
+
+    $usuarios = $mysqli->query("INSERT INTO usuario(id_Usuario, usuario, clave, rol) VALUES ('','$usuarioCliente','$pass','Usuario')");
  
-	if($conn->query($sql)===TRUE){
-        echo "Insert data";
+	if($usuarios===TRUE){
+        $cliente = $mysqli->query ("INSERT INTO cliente(id_Cliente, nombre, correo, id_Usuario) VALUES ('','$nombreCliente','$emailCliente','$mysqli->insert_id')");
+        if($cliente===TRUE){
+            echo "Insert data";
+        }
     }else{
         echo "failed";
     }
 
-    $conn->close();
+    $mysqli->close();
+    
 ?>
