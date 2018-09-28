62e01b0cc40f387cb5c4f969f7d9341576429447
AndreyPalma <andreypj98@gmail.com>
2018-09-27 23:33:24 -0600

    Seguridad para evitar SQL Injection en login

--- loginConsulta.php
+++ loginConsulta.php
@@ -1,27 +1,43 @@
 <?php
-require 'conexion.php';
-SESSION_START();
 
-$administrador = $mysqli->query("SELECT u.rol, a.nombreAdministrador FROM usuario AS u INNER JOIN administrador
-AS a ON u.id_Usuario = a.id_Usuario WHERE 
-u.usuario = '".$_POST['usuario']."' 
-AND u.clave = '".$_POST['clave']."' ");
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
+    require 'conexion.php';
+    SESSION_START();
 
-$usuarios = $mysqli->query("SELECT u.rol, c.nombreCliente, c.id_Cliente FROM usuario AS u INNER JOIN cliente
-AS c ON u.id_Usuario = c.id_Usuario
-WHERE u.usuario = '".$_POST['usuario']."'
-AND u.clave = '".$_POST['clave']."' ");
+    $mysqli->set_charset('utf8');
 
-if($usuarios->num_rows > 0){
-    $datos = $usuarios->fetch_assoc();
-    $_SESSION['usuario'] = $datos;
-    echo json_encode(array('error' => false, 'tipo' => $datos['rol']));
-}else if($administrador->num_rows > 0){
-    $datos = $administrador->fetch_assoc();
-    $_SESSION['usuario'] = $datos;
-    echo json_encode(array('error' => false, 'tipo' => $datos['rol']));
-}else{
-    echo json_encode(array('error' => true));
+    $usuario = $mysqli->real_escape_string($_POST['usuario']);
+    $clave = $mysqli->real_escape_string($_POST['clave']);
+    $administrador = $mysqli->prepare("SELECT u.rol, a.nombreAdministrador 
+    FROM usuario AS u INNER JOIN administrador
+    AS a ON u.id_Usuario = a.id_Usuario 
+    WHERE u.usuario = ? 
+    AND u.clave = ? ");
+    $cliente = $mysqli->prepare("SELECT u.rol, c.nombreCliente, c.id_Cliente 
+    FROM usuario AS u INNER JOIN cliente
+    AS c ON u.id_Usuario = c.id_Usuario
+    WHERE u.usuario = ?
+    AND u.clave = ? ");
+    $administrador->bind_param('ss',$usuario,$clave);
+    $administrador->execute();
+    $resultadoA = $administrador->get_result();
+    $cliente->bind_param('ss',$usuario,$clave);
+    $cliente->execute();
+    $resultadoC = $cliente->get_result();
+
+    if($resultadoA->num_rows == 1){
+        $datos = $resultadoA->fetch_assoc();
+        $_SESSION['usuario'] = $datos;
+        echo json_encode(array('error' => false, 'tipo' => $datos['rol']));
+    }else if($resultadoC->num_rows == 1){
+        $datos = $resultadoC->fetch_assoc();
+        $_SESSION['usuario'] = $datos;
+        echo json_encode(array('error' => false, 'tipo' => $datos['rol']));
+    }else{
+        echo json_encode(array('error' => true));
+    }
+    $administrador->close();
+    $cliente->close();
 }
 
 $mysqli->close();
