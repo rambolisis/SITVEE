19ad493cfe1d1a9a8f4ccd09ecc43ae223acee46
AndreyPalma <andreypj98@gmail.com>
2018-09-29 00:44:00 -0600

    Arreglos en fonts

--- index.php
+++ index.php
@@ -18,10 +18,10 @@ $Encabezado->generarHTML();
 </div>
     <div class="main" style="height:64%; width:100%; padding:3%;">
         <form id="formlg">
-            <h2>Inicia sesión en SITVEE</h2>
+            <h2><strong>Inicia sesión en SITVEE</strong></h2>
                 <input type="text" id="user" name="usuario" placeholder=" Usuario" pattern="[A-Za-z0-9_-]{1,15}" required/>
                 <input type="password" id="pass" name="clave" placeholder=" Contraseña" pattern="[A-Za-z0-9_-]{1,15}" required/>
-                <input type="submit" id="botonLogin" value="Iniciar Sesion"/>
+                <input type="submit" id="botonLogin" value="Iniciar Sesion" style="font-weight:bold;"/>
         </form>         
     </div>
 <?php $Pie->generarHTML(); ?>
