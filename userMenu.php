9a6b03f4430e336eccfd5b8598f61dedf36d2de7
AndreyPalma <andreypj98@gmail.com>
2018-11-05 18:59:15 -0600

    Tabla invitados mas larga

--- userMenu.php
+++ userMenu.php
@@ -167,7 +167,7 @@ $files = glob('QRimage/*'); //obtenemos todos los nombres de los ficheros
 				<input type="file" class="btn btn-outline-secondary" id="inputfile" style="padding: 3px;" name="Buscar">
 				<button class="btn btn-outline-secondary" type="button" id="viewfile">Cargar</button>
 		</div>
-	<div class="input-group mt-2 mb-2" id="divTabla" style="height:180px; width:100%; display: none;overflow-y:scroll;">
+	<div class="input-group mt-2 mb-2" id="divTabla" style="height:300px; width:100%; display: none;overflow-y:scroll;">
 		<table class="table table-hover table-dark" id="tableMain" style="width:100%;">
 			<thead>
 				<tr style="display: none;" id="fila">
